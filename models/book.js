30d92e67c9ac9c0041e340e3bbb467774e6e3278
lpjune <laurenporsini@gmail.com>
2019-11-10 20:34:33 -0600

    cleanup

--- models/book.js
+++ models/book.js
@@ -1,19 +1,17 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
 var BookSchema = new Schema({
-    title: {type: String, required: true},
-    summary: {type: String, required: true},
-    isbn: {type: String, required: true},
+  title: { type: String, required: true },
+  summary: { type: String, required: true },
+  isbn: { type: String, required: true }
 });
 
 // Virtual for this book instance URL.
-BookSchema
-.virtual('url')
-.get(function () {
-  return '/catalog/book/'+this._id;
+BookSchema.virtual("url").get(function() {
+  return "/catalog/book/" + this._id;
 });
 
 // Export model.
-module.exports = mongoose.model('Book', BookSchema);
+module.exports = mongoose.model("Book", BookSchema);
