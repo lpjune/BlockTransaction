9d2c1049a34990ef910f34a81190327058be1363
lpjune <laurenporsini@gmail.com>
2019-11-12 10:44:27 -0600

    adds first block if chain empty

--- app.js
+++ app.js
@@ -1,62 +1,73 @@
-require('dotenv').config();
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
 
-var compression = require('compression');
-var helmet = require('helmet');
+var compression = require("compression");
+var helmet = require("helmet");
 
 var app = express();
-
+var Block = require("./models/block");
 
 // Set up mongoose connection
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var mongoDB = process.env.DATABASE;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+let BlockChain = require("./blockchain/blockChain");
+
+let blockChain = new BlockChain();
+
+let hash = require("object-hash");
+
+let PROOF = 15;
+
+// set chain from fb, add first block if chain empty
+Block.find({})
+  .then(blocks => {
+
+    for(block in blocks) {
+      blockChain.chain.push(blocks)
+    }
+    console.log(blockChain.chain);
+    console.log(blockChain.chain.length);
+    if (blockChain.isEmpty()) {
+      blockChain.addNewBlock(null, "Alex", "Blah", "909", "12-24-2019");
+    }
+  })
+  .catch(() => {
+    console.log("Sorry! Something went wrong.");
+  });
 
 
-
-  let BlockChain = require("./blockchain/blockChain");
-
-  let blockChain = new BlockChain();
-
-  let hash = require('object-hash');
-
-  let PROOF = 15;
-
-  if(blockChain.isEmpty()) {
-    blockChain.addNewBlock(null, "Alex", "Blah", "909", "12-24-2019");
-  };
-
-  console.log("Chain : ", blockChain.chain);
 
 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(helmet());
 app.use(compression()); // Compress all routes
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -67,11 +78,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
